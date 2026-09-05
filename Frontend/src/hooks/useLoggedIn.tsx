9e11add8deb6f1ad4f8f15c1c1b2500354a06a4d
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-06 01:42:53 +0330

    Add useLoggedIn.tsx Hooks for authentication in Frontend

---
+++ Frontend/src/hooks/useLoggedIn.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const useLoggedIn = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/auth/", {
+                    credentials: "include",
+                });
+
+                setIsLoggedIn(res.ok);
+            } catch (error) {
+                setIsLoggedIn(false);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        checkAuth();
+    }, []);
+
+    return { isLoggedIn, loading };
+};
+
+export default useLoggedIn;
