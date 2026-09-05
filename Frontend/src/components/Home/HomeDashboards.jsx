13f15c5949f9e79e6ea4038abc20c6e481e63698
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-05 23:29:46 +0330

    Add hover to home button

--- Frontend/src/components/Home/HomeDashboards.jsx
+++ Frontend/src/components/Home/HomeDashboards.jsx
@@ -6,14 +6,14 @@ const HomeDashboards = () => {
 			<div className="flex py-10 flex-col items-center justify-center gap-10 md:flex-row-reverse md:gap-5">
 				<Link
 					href="/Persian/dashboard"
-					className="w-[50%] py-15  border-2 border-[#148DFF] bg-[linear-gradient(110deg,#0033B5,#0F152D)] rounded-3xl text-white font-black text-2xl max-sm:text-lg max-sm:w-[70%] md:w-[44%] md:py-20 md:text-3xl mx-auto "
+					className="w-[50%] py-15  border-2 border-[#148DFF] bg-[linear-gradient(110deg,#0033B5,#0F152D)] rounded-3xl text-white font-black text-2xl max-sm:text-lg max-sm:w-[70%] md:w-[44%] md:py-20 md:text-3xl mx-auto hover:shadow-[0px_0px_50px] hover:shadow-blue-500 transition-all duration-300"
 				>
 					<h1>داشبورد فارسی</h1>
 				</Link>
 
 				<Link
 					href="/English/dashboard"
-					className="w-[50%] py-15  border-2 border-[#148DFF] bg-[linear-gradient(250deg,#0033B5_1.15%,#0F152D_100%)] rounded-3xl text-white font-black text-2xl max-sm:text-lg max-sm:w-[70%] md:w-[44%] md:py-20 md:text-3xl mx-auto "
+					className="w-[50%] py-15  border-2 border-[#148DFF] bg-[linear-gradient(250deg,#0033B5_1.15%,#0F152D_100%)] rounded-3xl text-white font-black text-2xl max-sm:text-lg max-sm:w-[70%] md:w-[44%] md:py-20 md:text-3xl mx-auto hover:shadow-[0px_0px_50px] hover:shadow-blue-500 transition-all duration-300"
 				>
 					<h1>English Dashboard</h1>
 				</Link>
