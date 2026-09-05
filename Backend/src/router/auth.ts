c9110d4c044f5399952247e5f18c236f12450573
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-06 01:17:18 +0330

    auth routes edited and complete dashboard auth check

--- Backend/src/router/auth.ts
+++ Backend/src/router/auth.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express, { type NextFunction, type Request, type Response } from "express";
 import User from "../model/User";
 import bcrypt from "bcryptjs";
-
-
-
+import ResponseInterface from "../types/response";
+import authMiddleware from "../middleware/authMiddleware";
 const router = express.Router();
+
+router.get("/", authMiddleware, (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const Responses: ResponseInterface = { result: "succes", code: "AUTH_SUCCESSED" };
+		res.status(200).json(Responses);
+	} catch (error) {
+		next(error as string);
+	}
+});
 
 export default router;
