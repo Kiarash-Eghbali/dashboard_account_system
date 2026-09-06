960caa372b7f4615df4e70b35d223ffff611914a
Kiarash Eghbali <king.game.live89@gmail.com>
2026-09-06 18:14:30 +0330

    Add Style to changeLangButton and add hook to button, add Lang and dir to layout

--- Frontend/src/components/ChangeLangButton.tsx
+++ Frontend/src/components/ChangeLangButton.tsx
@@ -9,7 +9,9 @@ const ChangeLangButton: React.FC = (): JSX.Element => {
 
     return (
         <>
-            
+            <div onClick={toggleLang} className="w-10 h-10 text-xl rounded-full  flex items-center justify-center fixed  bottom-5 right-5 bg-blue-500 border-2 border-blue-300 hover:shadow-[0px_0px_10px] hover:shadow-blue-300 transition-all duration-300">
+                <h1 className="pt-1 select-none font-bold ">{ lang == "english" ? "FA" : "EN" }</h1>
+            </div>
         </>
     )
 }
--- Frontend/src/components/Shared/LoadingBox.tsx
+++ Frontend/src/components/Shared/LoadingBox.tsx
@@ -1,7 +1,7 @@
 import { JSX } from "react/jsx-runtime";
 
 const LoadingBox: React.FC = (): JSX.Element => {
-    return (
+    return ( 
         <>
             <div className="h-screen flex items-center justify-center">
                 <h1 className="text-5xl text-blue-500 font-bold animate-pulse">Loading...</h1>
